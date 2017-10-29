4d9b9559d2468c639fc0c21a88731a47294c20da
Araba Taylor <araba.taylor20@gmail.com>
2017-10-29 00:24:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,6 +12,7 @@ attr_accessor :name, :artist, :genre
   def initialize(name, artist, genre)
     @name=name
     @artist=artist
+    @genre=genre
     @@song_count += 1
     if @@genres.include?(genre) == false
       @@genres << genre
