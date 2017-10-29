599845942a415b9b94ae59b5f76dd8f024f24a6a
Araba Taylor <araba.taylor20@gmail.com>
2017-10-29 01:11:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ attr_accessor :name, :artist, :genre
 
   def self.genre_count
     binding.pry
-    @genre.reduce (@@genres(0)) {|genre, count| genre[counts] +=1; genre}
+    @genre.reduce(@@genres) {|genre, count| genre[counts] +=1; genre}
   end
 
   def self.artist_count
