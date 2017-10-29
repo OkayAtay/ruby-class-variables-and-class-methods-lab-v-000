00b32ca8b0eff9fc81646f5f1b3a9a74db6d23e7
Araba Taylor <araba.taylor20@gmail.com>
2017-10-29 00:07:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,15 @@
 class Song
+attr_accessor :name :artist :genre
+
+@@song_count = 0
+@@genres = []
 
   def initialize
+    @@song_count += 1
   end
+  
   def self.count
+    @@song_count
   end
 
   def self.artists
